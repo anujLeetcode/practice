2b7951aa875be05497ccce6e1c2ebbda98304ced
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-04-07 10:19:21 -0700

    Combination sum no duplicates

---
+++ combinationSumNoDuplicates.cpp
@@ -0,0 +1,37 @@
+Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+
+class Solution {
+public:
+    std::vector<std::vector<int> > combinationSum2(std::vector<int> &candidates, int target) {
+        std::sort(candidates.begin(), candidates.end());
+        std::vector<std::vector<int> > res;
+        std::vector<int> combination;
+        combinationSum2(candidates, target, res, combination, 0);
+        return res;
+    }
+private:
+    void combinationSum2(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i)
+            if (i == begin || candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combinationSum2(candidates, target - candidates[i], res, combination, i + 1);
+                combination.pop_back();
+            }
+    }
+};
