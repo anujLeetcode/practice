d806d299f5bf4ca75347d3d2e56ab87bb1294fde
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-04-21 15:51:40 -0700

    K smallest element in BST

---
+++ kSmallestBST.cpp
@@ -0,0 +1,12 @@
+/*
+k-smalles element in BST.
+*/
+int kthSmallest(TreeNode* root, int k) {
+    return find(root, k);
+}
+int find(TreeNode* root, int& k) {
+    if (root) {
+        int x = find(root->left, k);
+        return !k ? x : !--k ? root->val : find(root->right, k);
+    }
+}
