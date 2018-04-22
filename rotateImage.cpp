ece90e601de1a3ac9d2bf82a3ee2da69dd19e680
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-04-22 00:09:24 -0700

    Rotate image by 90 degrees

---
+++ rotateImage.cpp
@@ -0,0 +1,29 @@
+/*
+
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if(matrix[0].size() == 0 || matrix.size() == 0) return;
+        int n = matrix[0].size();
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < i; j++) {
+              swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        for(int i=0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+                   
+    }
+};
