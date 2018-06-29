3998c36f358f9fb5cd842115663d59a00354ea56
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-06-29 09:16:18 -0700

    Fixes to older commits

--- container_with_most_water.cpp
+++ container_with_most_water.cpp
@@ -5,4 +5,28 @@ Given n non-negative integers a1, a2, ..., an, where each represents a point at 
 
 Note: You may not slant the container and n is at least 2.
 */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int max_height = 0;
+        int min_height = 0;
+        
+        while(l < r) {
+            min_height = min(height[l], height[r]);
+            max_height = max(max_height, min_height * (r - l));
+            
+            //int i = l+1;
+            //int j = r-1;
 
+            while(height[l] <= min_height && l < r) l++;
+            //l = i;
+            
+            while(height[r] <= min_height && l < r) r--;
+            //r = j;       
+        }
+        
+        return max_height;
+    }
+};
--- firstMissingPositive.cpp
+++ firstMissingPositive.cpp
@@ -2,6 +2,30 @@
 /*
 Given an unsorted integer array, find the first missing positive integer.
 
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+        while (i < n)
+        {
+            if (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i]-1]);
+            else
+                i++;
+        }
+        for (i = 0; i < n; ++i)
+            if (nums[i] != (i+1))
+                return i+1;
+        return n+1;
+    }
+};
+
+
+
+
+
+
 For example,
 Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
