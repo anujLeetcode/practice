eaa746c6071c82c6aa77cf60cc395f5e28f2a303
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-06-29 09:31:02 -0700

    COunt primes

---
+++ countPrimes.cpp
@@ -0,0 +1,33 @@
+/*
+204. Count Primes
+DescriptionHintsSubmissionsDiscussSolution
+Pick One
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+*/
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> isprime(n+1, true);
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            //cout << isprime[i] << endl;
+            if(isprime[i] == true) {
+                count++;
+                //cout << count << endl;
+                for(int j = 2; i*j <n ; j++) {
+                    isprime[i*j] = false;
+                }
+                
+            }
+        }
+        return count;
+    }
+};
