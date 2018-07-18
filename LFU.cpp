04d50202b3e0691c131a70e39e42ed4723428bf3
Anuj Phadke <anujphadke@aphadke-MBP-15.local>
2018-07-18 08:46:13 -0700

    LFU cache

---
+++ LFU.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<pair>
+
+using namespace std;
+
+class solution {
+
+private:
+  // key -> value, freq, iterator
+  unordered_map<int, pair<int, list<int>::iterator> > cache;
+
+ // freq -> list bucket
+  unordered_map<int, list> iter_map;
+
+
+  
+
+
+}
